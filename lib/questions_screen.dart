5e00928af54bde45bd5051b5e396034efd7a3f2e
Ekraj Ghimire <56933759+ManishGhimire127@users.noreply.github.com>
2024-10-29 23:36:07 +0530

    paddings

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -16,28 +16,33 @@ class _QuestionScreenState extends State<QuestionScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-                color: Colors.pink, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-          // AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
-          // const SizedBox(height: 10),
-          // AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
-          // const SizedBox(height: 10),
-          // AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
-          // const SizedBox(height: 10),
-          // AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                  color: Colors.pink, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+            // AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+            // const SizedBox(height: 10),
+            // AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+            // const SizedBox(height: 10),
+            // AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+            // const SizedBox(height: 10),
+            // AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+          ],
+        ),
       ),
     );
   }
