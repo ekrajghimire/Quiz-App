ed9a4c0520b8a402efe0f3126831619fa2298fbd
Ekraj Ghimire <56933759+ManishGhimire127@users.noreply.github.com>
2024-11-13 19:04:59 +0530

    combining columns and rows

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/data/questions.dart';
+import 'package:quizapp/question_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({
@@ -38,7 +39,7 @@ class ResultScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of questions and answers.'),
+            QuestionSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
